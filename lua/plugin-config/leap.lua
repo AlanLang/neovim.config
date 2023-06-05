048719eb538b59c1cfcb749e139d4ba0b203725f
alan <alan@fanruan.com>
2023-06-05 20:37:36 +0800

    update config

--- lua/plugin-config/leap.lua
+++ lua/plugin-config/leap.lua
@@ -1,5 +1,7 @@
 return {
   "ggandor/leap.nvim",
+  lazy = true,
+  keys = { "E", "R", "W", "dE", "dR", "yE", "yR", "cE", "cR" },
   config = function()
     require('leap').add_default_mappings()
   end
