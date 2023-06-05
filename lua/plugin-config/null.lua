db01f98d3a10affea39fa0cd5d1ccdfb1b1ca4f8
alan <alan@fanruan.com>
2023-06-05 10:11:53 +0800

    add esint

--- lua/plugin-config/null.lua
+++ lua/plugin-config/null.lua
@@ -1,9 +1,12 @@
 return {
   "jose-elias-alvarez/null-ls.nvim",
-  dependencies = {"nvim-lua/plenary.nvim"},
-  config = function ()
+  dependencies = { "nvim-lua/plenary.nvim" },
+  config = function()
     local null_ls = require("null-ls")
     local formatting = null_ls.builtins.formatting
+    local diagnostics = null_ls.builtins.diagnostics
+    local code_actions = null_ls.builtins.code_actions
+    local ag = vim.api.nvim_create_augroup
     null_ls.setup({
       debug = false,
       sources = {
@@ -13,7 +16,8 @@ return {
         -- StyLua
         formatting.stylua,
         -- frontend
-        formatting.prettier.with({ -- 只比默认配置少了 markdown
+        formatting.prettier.with({
+          -- 只比默认配置少了 markdown
           filetypes = {
             "javascript",
             "javascriptreact",
@@ -30,9 +34,14 @@ return {
         }),
         -- formatting.fixjson,
         -- formatting.black.with({ extra_args = { "--fast" } }),
+        diagnostics.eslint_d.with({
+          prefer_local = "node_modules/.bin",
+        }),
+        code_actions.eslint_d,
       },
       -- 保存自动格式化
       on_attach = function(client, bufnr)
+        local augroup = ag("LspFormatting", { clear = false })
         if client.supports_method("textDocument/formatting") then
           vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
           vim.api.nvim_create_autocmd("BufWritePre", {
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -18,64 +18,65 @@ local autopairs = require("plugin-config.autopairs")
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	})
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
 
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-	-- 主题
-	"AlanLang/oceanic-next",
-	-- 设置透明
-	"xiyaowong/nvim-transparent",
-	treesitter,
-	-- 输入法自动切换
-	"ybian/smartim",
-	-- 弹出式窗口
-	telescope,
-	-- 文件树
-	nvim_tree,
-	-- 状态栏
-	lualine,
-	-- 命令中心
-	command_center,
-	-- 启动页面
-	dashboard,
-	-- 项目列表
-	project,
-	-- LSP 管理
-	mason,
-	mason_lspconfig,
-	lspconfig,
-	-- 平滑滚动
-	neoscroll,
-	-- 光标平滑移动
-	smooth_cursor,
-	-- 目标快速操作
-	"wellle/targets.vim",
-	-- 同缩进快速操作
-	"michaeljsmith/vim-indent-object",
-	-- git 信息
-	"f-person/git-blame.nvim",
-	-- {
-	--   "github/copilot.vim",
-	-- },
-	-- 注释
-	comment,
-	-- 代码补全
-	cmp,
-	-- 快速选择
-	"easymotion/vim-easymotion",
-	-- 多光标选择
-	"mg979/vim-visual-multi",
-	autopairs,
-	lspkind,
-	null_ls,
+  -- 主题
+  "AlanLang/oceanic-next",
+  -- 设置透明
+  "xiyaowong/nvim-transparent",
+  treesitter,
+  -- 输入法自动切换
+  "ybian/smartim",
+  -- 弹出式窗口
+  telescope,
+  -- 文件树
+  nvim_tree,
+  -- 状态栏
+  lualine,
+  -- 命令中心
+  command_center,
+  -- 启动页面
+  dashboard,
+  -- 项目列表
+  project,
+  -- LSP 管理
+  mason,
+  mason_lspconfig,
+  lspconfig,
+  -- 平滑滚动
+  neoscroll,
+  -- 光标平滑移动
+  smooth_cursor,
+  -- 目标快速操作
+  "wellle/targets.vim",
+  -- 同缩进快速操作
+  "michaeljsmith/vim-indent-object",
+  -- git 信息
+  "f-person/git-blame.nvim",
+  -- {
+  --   "github/copilot.vim",
+  -- },
+  -- 注释
+  comment,
+  -- 代码补全
+  cmp,
+  -- 快速选择
+  "easymotion/vim-easymotion",
+  -- 多光标选择
+  "mg979/vim-visual-multi",
+  autopairs,
+  lspkind,
+  null_ls,
+  "wakatime/vim-wakatime"
 })
