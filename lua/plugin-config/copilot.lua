48263a0910a3236b55b9421f6855468b1f6a811a
alan <alan@fanruan.com>
2023-06-05 11:02:54 +0800

    update copilog

---
+++ lua/plugin-config/copilot.lua
@@ -0,0 +1,21 @@
+return {
+  "zbirenbaum/copilot.lua",
+  cmd = "Copilot",
+  event = "InsertEnter",
+  dependencies = "zbirenbaum/copilot-cmp",
+  config = function()
+    require("copilot").setup({
+      panel = {
+        enabled = false
+      },
+      suggestion = {
+        auto_trigger = true,
+      }
+    })
+    require("copilot_cmp").setup({
+      formatters = {
+        insert_text = require("copilot_cmp.format").remove_existing
+      },
+    })
+  end
+}
