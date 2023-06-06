357e0b046e4b4dcdf9f5d85bfea4046fe8071f40
alan <alan@fanruan.com>
2023-06-06 12:38:03 +0800

    change key map

--- lua/plugin-config/leap.lua
+++ lua/plugin-config/leap.lua
@@ -1,14 +1,14 @@
 return {
   "ggandor/leap.nvim",
   lazy = true,
-  keys = { "E", "R", "W", "dE", "dR", "yE", "yR", "cE", "cR" },
+  keys = { "s", "S", "W", "ds", "dS", "ys", "yS", "cs", "cS" },
   config = function()
     require("leap").opts.highlight_unlabeled_phase_one_targets = true
     require("leap").opts.safe_labels = { 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'q', 'w', 'e', 'r', 't', 'y', 'u',
       'i', 'o', 'p' }
     -- leap.add_default_mappings()
-    vim.keymap.set({ "x", "o", "n" }, "E", "<Plug>(leap-forward-to)")
-    vim.keymap.set({ "x", "o", "n" }, "R", "<Plug>(leap-backward-to)")
+    vim.keymap.set({ "x", "o", "n" }, "s", "<Plug>(leap-forward-to)")
+    vim.keymap.set({ "x", "o", "n" }, "S", "<Plug>(leap-backward-to)")
     vim.keymap.set({ "x", "o", "n" }, "W", "<Plug>(leap-from-window)")
   end
 }
