c82cd19c530056ff367462ffbd29c71b9c17e8ec
alan <langwdalan@gmail.com>
2023-06-13 09:54:01 +0800

    update

--- lua/plugin-config/gomove.lua
+++ lua/plugin-config/gomove.lua
@@ -1,7 +1,7 @@
 return {
   "booperlv/nvim-gomove",
   layz = true,
-  keys = { "<left>", "<down>", "<up>", "<right>", "<C-h>", "<C-j>", "<C-k>", "<C-l>" },
+  event = "VeryLazy",
   config = function()
     require("gomove").setup({
       map_defaults = false,
