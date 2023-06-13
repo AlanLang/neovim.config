4cd8c5981e470cba27cffa19d1ddb6d1d8c31e78
alan <alan@fanruan.com>
2023-06-13 13:25:54 +0800

    update config

--- lua/plugin-config/lualine.lua
+++ lua/plugin-config/lualine.lua
@@ -56,6 +56,8 @@ local config = {
 return {
   "nvim-lualine/lualine.nvim",
   dependencies = { "nvim-tree/nvim-web-devicons" },
+  lazy = true,
+  event = "VeryLazy",
   config = function()
     require("lualine").setup(config)
   end
