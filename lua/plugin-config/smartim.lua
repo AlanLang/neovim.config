16b425fd50087299d9a2a8f38ee91e4eac0d2591
alan <alan@fanruan.com>
2023-06-13 15:38:42 +0800

    update config

---
+++ lua/plugin-config/smartim.lua
@@ -0,0 +1,6 @@
+-- 自动切换输入法
+return {
+  "ybian/smartim",
+  lazy = true,
+  event = "InsertEnter",
+}
---
+++ lua/plugin-config/wakatime.lua
@@ -0,0 +1,6 @@
+-- 记录代码时间
+return {
+  "wakatime/vim-wakatime",
+  lazy = true,
+  event = "VeryLazy"
+}
