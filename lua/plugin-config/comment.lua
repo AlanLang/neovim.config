d541ed6a98ada32edb4289669b24ed9a47acc89a
alan <langwdalan@gmail.com>
2023-06-17 19:23:13 +0800

    update config

--- lua/plugin-config/comment.lua
+++ lua/plugin-config/comment.lua
@@ -1,7 +1,7 @@
 return {
   "numToStr/Comment.nvim",
   lazy = true,
-  keys = { "gcc", "gbc", "gc", "gb" },
+  keys = { "gcc", "gbc", { "gc", mode = { "v" } }, { "gb", mode = { "v" } } },
   config = function()
     require("Comment").setup()
   end,
