789dd51eee4b01fb4ba0edd111ce384a126b6194
alan <langwdalan@gmail.com>
2023-07-15 22:41:30 +0800

    fix: css 代码提示的问题

--- lua/lsp/common-config.lua
+++ lua/lsp/common-config.lua
@@ -10,7 +10,6 @@ end
 
 -- 禁用格式化功能，交给专门插件插件处理
 M.disableFormat = function(client)
-  client.server_capabilities.semanticTokensProvider = nil
   if vim.fn.has("nvim-0.8") == 1 then
     client.server_capabilities.documentFormattingProvider = false
     client.server_capabilities.documentRangeFormattingProvider = false
@@ -21,7 +20,7 @@ M.disableFormat = function(client)
 end
 
 -- M.capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
--- M.capabilities = require("cmp_nvim_lsp").default_capabilities()
+M.capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 M.flags = {
   debounce_text_changes = 150,
--- lua/lsp/config/css.lua
+++ lua/lsp/config/css.lua
@@ -1,11 +1,36 @@
-local capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
-capabilities.textDocument.completion.completionItem.snippetSupport = true
-
+local common = require("lsp.common-config")
+local opts = {
+  capabilities = common.capabilities,
+  flags = common.flags,
+  on_attach = function(client, bufnr)
+    common.disableFormat(client)
+    common.keyAttach(bufnr)
+  end,
+  settings = {
+    css = {
+      validate = true,
+      -- tailwindcss
+      lint = {
+        unknownAtRules = "ignore",
+      },
+    },
+    less = {
+      validate = true,
+      lint = {
+        unknownAtRules = "ignore",
+      },
+    },
+    scss = {
+      validate = true,
+      lint = {
+        unknownAtRules = "ignore",
+      },
+    },
+  },
+}
 
 return {
   on_setup = function(server)
-    server:setup({
-      capabilities = capabilities,
-    })
+    server.setup(opts)
   end,
 }
